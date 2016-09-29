e4833ef4c35e96ff635b20335e358e995c128cdb
Glenn Doren <glenn.doren@gmail.com>
2016-09-29 15:59:44 -0700

    More stuff :)

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 // Add your requirements
+
+//id 09ff0371-087e-4cb4-bde8-bf8ed549b5d4
+//secret FTe7CBjTq4mad8LRKQaqcXq
+
 var restify = require('restify'); 
 var builder = require('botbuilder'); 
+
+var appId = process.env.MY_APP_ID || "Missing app ID";
+var appPassword = process.env.MY_APP_PASSWORD || "Missing app password";
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -11,7 +18,7 @@ server.listen(process.env.PORT || 3000, function()
 
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: 'YourAppId', appPassword: 'YourAppPassword' }); 
+({ appId: process.env.MY_APP_ID, appPassword: process.env.MY_APP_PASSWORD }); 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
