d0dc2f2450ea861421fcfdaee3ccc70b9e8a5aa8
Glenn Doren <glenn.doren@gmail.com>
2016-09-29 18:05:14 -0700

    added prompts.js

---
+++ prompts.js
@@ -0,0 +1,12 @@
+module.exports = {
+    helpMessage: "CasinoBot lets you enjoy the pleasures of a casino via chat. Your options are:\n\n" +
+    "* 'lets play CRAPS'\n" +
+    "* 'FLIP a coin'\n" +
+    "* 'DEPOSIT more money'\n" +
+    "* 'WITHDRAW money'\n" +
+    "* 'step AWAY from table'\n" +
+    "* 'LEAVE casino'\n" +
+    "\nEnjoy your stay!",
+    flipResult: "Coin flip is %(coindSideUp)s. You %(result)s $%(amount)d.",
+    status: "money: $%(money)d"
+};
